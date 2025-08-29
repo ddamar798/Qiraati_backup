a559e418eecdf4b3d09ea1c2bdefacf6a134527b
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-08-29 20:05:16 +0700

    backup all

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,45 +1,5 @@
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
+    id("com.android.application") version "8.9.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("dev.flutter.flutter-gradle-plugin")
-}
-
-android {
-    namespace = "com.example.qiraati"
-    compileSdk = 34
-
-    defaultConfig {
-    applicationId = "com.example.qiraati"
-    minSdk = 21
-    targetSdk = 34
-
-    val flutterVersionCode = project.findProperty("flutter.versionCode") as? String ?: "1"
-    val flutterVersionName = project.findProperty("flutter.versionName") as? String ?: "1.0"
-
-    versionCode = flutterVersionCode.toInt()
-    versionName = flutterVersionName
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-
-    // Jika kamu tidak pakai multidex, hapus baris multiDexEnabled
-    // Tapi jika target SDK 34+, biasanya tidak perlu
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    // Hanya tambahkan jika kamu benar-benar pakai multidex
-    // implementation("androidx.multidex:multidex:2.0.1")
 }
